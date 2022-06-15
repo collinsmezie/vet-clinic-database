766551c2349108cae1f2244bdf5e7d9c38d4f3df
collinsmezie <collinschimezieobinwa@gmail.com>
2022-06-15 14:26:50 +0400

    modified sql files to create database table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,19 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM animals WHERE name LIKE '%mon%';
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-11-15' AND '2019-11-15';
+
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered = TRUE;
+
+SELECT * FROM animals WHERE name NOT LIKE '%Gabumon';
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 OR weight_kg = 10.4 OR weight_kg = 17.3;
