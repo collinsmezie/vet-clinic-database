1667c9bc8b422caa5902034729ca10e897552798
collinsmezie <collinschimezieobinwa@gmail.com>
2022-06-18 18:28:10 +0400

    modified database implementing multiple tables

--- data.sql
+++ data.sql
@@ -1,16 +1,16 @@
 /* Populate database with sample data. */
 
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
--- VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
+VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
 
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
--- VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
+VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8.0);
 
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
--- VALUES ('Pikachu','2021-01-07', 1, FALSE, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
+VALUES ('Pikachu','2021-01-07', 1, FALSE, 15.04);
 
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
--- VALUES ('Devimon', '2017-05-12', 5, TRUE, 11.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
+VALUES ('Devimon', '2017-05-12', 5, TRUE, 11.0);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
 VALUES ('Charmander', '2020-02-08', 0, FALSE, -11);
@@ -33,5 +33,75 @@ VALUES ('Blossom', '1998-10-13', 3, TRUE, 17 );
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  
 VALUES ('Ditto', '2022-05-14', 4, TRUE, 22 );
 
+-------------------------------------------------------------------
+
+INSERT INTO owners (full_name, age)  
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody POnd', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)  
+VALUES ('Pokemon'),
+('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value 
+-- If the name ends in "mon" it will be Digimon
+UPDATE animals 
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+-- Set All other animals to Pokemon
+UPDATE animals 
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+UPDATE animals 
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+-- Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals 
+SET owner_id = 2
+WHERE name = 'Gabumon';
+
+UPDATE animals 
+SET owner_id = 2
+WHERE name = 'Pikachu';
+
+-- Bob owns Devimon and Plantmon.
+UPDATE animals 
+SET owner_id = 3
+WHERE name = 'Devimon';
+
+UPDATE animals 
+SET owner_id = 3
+WHERE name = 'Plantmon';
+
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE animals 
+SET owner_id = 4
+WHERE name = 'Charmander';
+
+UPDATE animals 
+SET owner_id = 4
+WHERE name = 'Squirtle';
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Blossom';
+
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE animals 
+SET owner_id = 5
+WHERE name = 'Angemon';
+
+UPDATE animals 
+SET owner_id = 5
+WHERE name = 'Boarmon';
 
 
--- schema.sql
+++ schema.sql
@@ -9,7 +9,61 @@ CREATE TABLE animals(
    weight_kg      DECIMAL NOT NULL,
 );
 
-
 -- ADD species column to animals table schema
 
 ALTER TABLE animals ADD species VARCHAR;
+
+---------------------------------------------------------------------------------------
+
+--Modify animals table--
+
+--create species table
+CREATE TABLE species(
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+   name  VARCHAR  NOT NULL
+);
+
+--create owners table
+CREATE TABLE owners(
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+   full_name  VARCHAR  NOT NULL,
+   age        INT  NOT NULL
+);
+
+-- ALTER TABLE owners
+-- RENAME COLUMN id TO animal_owner_id;
+
+
+-- Make sure that id is set as autoincremented PRIMARY KEY
+ALTER TABLE animals
+ADD COLUMN id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL;
+
+
+--Remove column species
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals 
+ADD COLUMN species_id INT, 
+ADD FOREIGN KEY(species_id) REFERENCES species(id);
+
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals 
+ADD COLUMN owner_id INT, 
+ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+
+-- Modify your inserted animals so it includes the species_id value 
+-- If the name ends in "mon" it will be Digimon
+UPDATE animals 
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+-- Set All other animals to Pokemon
+UPDATE animals 
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+ALTER TABLE animals 
+DROP COLUMN id;
